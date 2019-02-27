05199d9f11fa4aa567554dac298a9e9675b89207
Ben <benlorantfy@gmail.com>
2019-02-27 08:49:55 -0500

    Fix linting and add lint step

--- src/rules/CommitLintRule.ts
+++ src/rules/CommitLintRule.ts
@@ -1,6 +1,7 @@
 import { CommitInfo } from "../types"
 
 export abstract class CommitLintRule {
+  // tslint:disable-next-line no-empty
   constructor(info: CommitInfo) {}
   public abstract fail(info: CommitInfo): boolean
 }
